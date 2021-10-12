f637d55c3c129d7a941757d83196fd89b9cb10a4
Franklin Almeida Campos <81038899+franklindrw@users.noreply.github.com>
2021-10-12 18:29:04 -0300

    adicionando paginas p/ produtos

--- js/app.js
+++ js/app.js
@@ -16,4 +16,21 @@ document.querySelectorAll(".linkPage").forEach(link =>{
                 .then(html => components.innerHTML = html)
         }
     }
-)
+);
+
+
+//Preview das Imagens enviadas no cadastro
+function lerImagem(){
+    if (this.files && this.files[0]){
+
+        //faz a leitura da imagem enviada e carrega na div preview
+        var file = new FileReader();
+        file.onload  = function(e){
+            document.getElementById("preview").src = e.target.result;
+        };
+
+        file.readAsDataURL(this.files[0]);
+    }
+}
+//mapeia quando é colocado alguma imagem no input e inicia a function
+document.getElementById("uploadImagem").addEventListener("change", lerImagem, false);
